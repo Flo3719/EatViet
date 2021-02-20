ec8bb380b04627a1e7c6ad2ee2c536fc5bb43c04
Flo3719 <florian-fahrenholz@web.de>
2021-02-20 13:49:29 +0100

    show streetview and interactive map

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,10 @@
 module RestaurantsHelper
+  def google_map(center)
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17&key="
+  end
+  def street_view(center)
+    "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=#{center}
+    &fov=80&heading=0&pitch=0
+    &key="
+  end
 end
