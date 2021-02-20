0baa8e0e2cb0feef5dd12d05d9a0066ba82c9454
Flo3719 <florian-fahrenholz@web.de>
2021-02-20 14:25:21 +0100

    Use env for mapsAPI

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,10 +1,10 @@
 module RestaurantsHelper
   def google_map(center)
-    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17&key="
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17&key=" + Rails.application.credentials.maps_api
   end
   def street_view(center)
     "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=#{center}
     &fov=80&heading=0&pitch=0
-    &key="
+    &key=" + Rails.application.credentials.maps_api
   end
 end
